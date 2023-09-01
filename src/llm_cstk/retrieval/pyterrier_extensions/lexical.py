5a0a0e0cf3abce9f30c929abc789c1119cb4b230
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2023-09-01 17:34:03 +0200

    Update lexical ranker

--- src/llm_cstk/retrieval/pyterrier_extensions/lexical.py
+++ src/llm_cstk/retrieval/pyterrier_extensions/lexical.py
@@ -16,14 +16,14 @@ class LexicalPTRanker(_PTRanker, _Singleton):
         self.reranking_model: Optional[LexicalSearch] = reranking_model
     
     def _get_model(
-            self, model: LexicalSearch, corpus: str, metadata: bool = False
+            self, model: LexicalSearch, corpus: str, chunk_doc: bool = False, metadata: bool = False
     ) -> pt.Transformer:
         # Get model id
-        model_id: str = str(np.uint(hash(f'{model}_{corpus}')))
+        model_id: str = str(np.uint(hash(f'{model}_{corpus}_{CHUNK_AFFIX}' if chunk_doc else f'{model}_{corpus}')))
         # Check whether the model is not already in cache
         if model_id not in self._model_cache:
             # Load index
-            pt_indexer: pt.Indexer = pt.DFIndexer(self.get_index_dir_path(corpus))
+            pt_indexer: pt.Indexer = pt.DFIndexer(self.get_index_dir_path(corpus, chunk_doc=chunk_doc))
             pt_index_ref = pt.IndexRef.of(pt_indexer.path)
             pt_index: pt.IndexRef = pt.IndexFactory.of(pt_index_ref)
             # Build retriever model
@@ -36,26 +36,30 @@ class LexicalPTRanker(_PTRanker, _Singleton):
         return self._model_cache[model_id]
 
     def get_ranking_model(
-            self, corpus: str, reranking: bool = False, cutoff: bool = True, metadata: bool = False
+            self, corpus: str,
+            chunk_doc: bool = False,
+            reranking: bool = False,
+            cutoff: bool = True,
+            metadata: bool = False
     ) -> pt.Transformer:
         if cutoff:
             return self._get_model(
-                self.ranking_model, corpus, metadata=metadata or reranking
+                self.ranking_model, corpus, chunk_doc=chunk_doc, metadata=metadata or reranking
             ) % (self.ranking_cutoff if not reranking else self.reranking_cutoff)
         else:
             return self._get_model(self.ranking_model, corpus, metadata=metadata or reranking)
 
-    def get_reranking_model(self, corpus: str, metadata: bool = False) -> pt.Transformer:
-        return self._get_model(self.reranking_model, corpus, metadata=metadata)
+    def get_reranking_model(self, corpus: str, chunk_doc: bool = False, metadata: bool = False) -> pt.Transformer:
+        return self._get_model(self.reranking_model, corpus, chunk_doc=chunk_doc, metadata=metadata)
 
-    def get_scoring_model(self, corpus: str, txt_col: str = TEXT, ranking: bool = True):
+    def get_scoring_model(self, corpus: str, chunk_doc: bool = False, txt_col: str = TEXT, ranking: bool = True):
         # Get model id
         model = self.ranking_model if ranking else self.reranking_model
-        model_id: str = str(np.uint(hash(f'{model}_{corpus}')))
+        model_id: str = str(np.uint(hash(f'{model}_{corpus}_{CHUNK_AFFIX}' if chunk_doc else f'{model}_{corpus}')))
         # Check whether the model is not already in cache
         if model_id not in self._scoring_model_cache:
             # Load index
-            pt_indexer: pt.Indexer = pt.DFIndexer(self.get_index_dir_path(corpus))
+            pt_indexer: pt.Indexer = pt.DFIndexer(self.get_index_dir_path(corpus, chunk_doc=chunk_doc))
             pt_index_ref = pt.IndexRef.of(pt_indexer.path)
             pt_index: pt.IndexRef = pt.IndexFactory.of(pt_index_ref)
             # Build retriever model
@@ -69,5 +73,5 @@ class LexicalPTRanker(_PTRanker, _Singleton):
 
         return self._scoring_model_cache[model_id]
 
-    def get_index_path(self, corpus: str) -> str:
-        return PTDocManager.get_lexical_index_path(self.data_dir_path, corpus)
+    def get_index_path(self, corpus: str, chunk_doc: bool = False) -> str:
+        return PTDocManager.get_lexical_index_path(self.data_dir_path, corpus, chunk_doc=chunk_doc)
