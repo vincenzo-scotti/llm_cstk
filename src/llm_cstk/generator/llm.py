9bad7f0d4e1a8d550e204827289f1c970e09fe9e
Vincenzo Scotti <vincenzo.scotti@polimi.it>
2023-09-14 16:44:48 +0200

    Update LLM interface

--- src/llm_cstk/generator/llm.py
+++ src/llm_cstk/generator/llm.py
@@ -11,22 +11,20 @@ class LLMAPI(_Singleton):
             self,
             url: str,
             *args,
-            generate_params: Optional[Dict] = None,
-            instructions: Optional[Dict[str, str]] = None,
-            templates: Optional[Dict[str, str]] = None,
+            generate_params: Optional[Dict[Task, Dict]] = None,
+            instructions: Optional[Dict[Task, str]] = None,
+            templates: Optional[Dict[Task, Dict]] = None,
             **kwargs
     ):
         super().__init__(*args, **kwargs)
         #
         self.url: str = url
-        self.generate_params: Dict = generate_params if generate_params is not None else dict()
-        self.instructions: Dict = instructions if instructions is not None else dict()
-        self.templates: Dict = templates if templates is not None else dict()
+        self.generate_params: Dict[Task, Dict] = generate_params if generate_params is not None else dict()
+        self.instructions: Dict[Task, str] = instructions if instructions is not None else dict()
+        self.templates: Dict[Task, Dict] = templates if templates is not None else dict()
 
-    def _prepare_completions_input(self, utterances: List[Dict[str, str]]):
-        return {
-            MESSAGES: [{ROLE: u[SPEAKER], CONTENT: u[TEXT]} for u in utterances], **self.generate_params
-        }
+    def _prepare_completions_input(self, utterances: List[Dict[str, str]], **generate_params):
+        return {MESSAGES: [{ROLE: u[SPEAKER], CONTENT: u[TEXT]} for u in utterances], **generate_params}
 
     @staticmethod
     def _decode_completions_output(http_response: requests.Response) -> Optional[str]:
@@ -35,9 +33,9 @@ class LLMAPI(_Singleton):
         else:
             return None
 
-    def completion(self, utterances: List[Dict[str, str]]) -> str:
+    def completion(self, utterances: List[Dict[str, str]], **generate_params) -> str:
         request_url: str = urljoin(self.url, COMPLETIONS_PATH)
-        request_data: str = json.dumps(self._prepare_completions_input(utterances))
+        request_data: str = json.dumps(self._prepare_completions_input(utterances, **generate_params))
         http_response: requests.Response = requests.post(request_url, headers=HEADERS, data=request_data)
         completion: str = self._decode_completions_output(http_response)
 
